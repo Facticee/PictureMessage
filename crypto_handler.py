1df7bb2ddeeac7c4e72904af6de49d897e75f46a
Facticee <timon.eliah.plum@gmail.com>
2026-07-31 19:13:42 +0200

    first file - encryption function

---
+++ crypto_handler.py
@@ -0,0 +1,14 @@
+import base64
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+def get_key(pw, salt):
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=480000)
+    return base64.urlsafe_b64encode(kdf.derive(pw.encode()))
+
+def verschlüsseln(text, pw, salt):
+    f = Fernet(get_key(pw, salt))
+    res = f.encrypt(text.encode())
+    return res
